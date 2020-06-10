49d74ec168a8037962f9909a9c73038e2b8b1d9a
Egor <bifrost.step@gmail.com>
2020-06-10 21:58:20 +0300

    Исправил формы

--- blueprints/forms/forms_users.py
+++ blueprints/forms/forms_users.py
@@ -12,22 +12,16 @@ class FormAddUser(FlaskForm):
     name_reg = StringField('Your name or nickname', validators=[DataRequired(message='This field is required')])
     email_reg = StringField('Your email address', validators=[DataRequired(message='This field is required'), Email(message='Invalid email address')])
     password_reg = PasswordField('Set password', validators=[DataRequired(message='This field is required'),
-                                                         Length(min=5, message='The password must contain at least 5 characters')])
+                                                             Length(min=5, message='The password must contain at least 5 characters')])
+    password_again_reg = PasswordField('Repeat password', [DataRequired(message='This field is required'), EqualTo('password_reg', message='Passwords must match')])
     show_password_reg = BooleanField('Show password')
-    password_again_reg = PasswordField('Repeat password', [DataRequired(message='This field is required'), EqualTo('password', message='Passwords must match')])
     remember_reg = BooleanField('Remember me')
     submit_reg = SubmitField('Send')
 
-    def validate_email(self, field):
+    def validate_email_reg(self, field):
         session = db_session.create_session()
         if session.query(User.email).filter(User.email == field.data).first():
             raise ValidationError("This email already exists")
-
-    def validate_photo(self, field):
-        if field.data:
-            filename = secure_filename(field.data.filename)
-            if filename.rsplit('.', 1)[1] not in ('png', 'jpeg', 'jpg'):
-                raise ValidationError("Invalid extension for photos, use .png or. jpeg(.jpg)")
 
 
 class FormEditUser(FlaskForm):

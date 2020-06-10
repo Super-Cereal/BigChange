08a7ae5c9d1740577b61f015e3d1b3806de7daff
Pavel <palllkaignatev@Gmail.com>
2020-06-10 20:25:44 +0300

    Изменение формы

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $('.menu_ul').fadeOut(1000);
-    let textUserQuestion = document.getElementById('email');
+    let textUserQuestion = document.getElementById('email_reg');
         textUserQuestion.focus();
+    
 
 });

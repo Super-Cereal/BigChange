5f4e1cbac9eef73107f5441ae5e7ee3614ca5ad4
Pavel <palllkaignatev@Gmail.com>
2020-06-11 00:24:00 +0300

    изменение формы

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
-    $('.menu_ul').fadeOut(1000);
-    let textUserQuestion = document.getElementById('email_reg');
-        textUserQuestion.focus();
-    
+    $('.menu_ul').fadeOut(2000);
+    function explode(){
+        $('#hamb').fadeIn(1000);
+      }
+      setTimeout(explode, 2001);
+    let textUserQuestion = document.getElementById('email_log');
+                textUserQuestion.focus();
+    $('.vt').on('click', function(){
+            $('.modal_reg').fadeOut('slow')
+            $('.modal_log').fadeIn(500)
+            
+            
+          });
+    $('.sz').on('click', function(){
+            $('.modal_log').fadeOut('slow')
+            $('.modal_reg').fadeIn(500)
+            let textUserQuestion = document.getElementById('email_reg');
+                textUserQuestion.focus();
+          });
 
 });

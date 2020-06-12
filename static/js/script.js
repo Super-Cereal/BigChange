a1c36e3dc30eca07649f8aaa9ba64cc63485d86a
Pavel <palllkaignatev@Gmail.com>
2020-06-12 05:52:53 +0300

    новый шрифт

--- static/js/script.js
+++ static/js/script.js
@@ -1,24 +1,30 @@
 $(document).ready(function(){
-    $('.menu_ul').hide();
+  $('.menu_ul').hide();
 
+  const textUserQuestion = document.getElementById('email_log');
+  const textUserQuestion2 = document.getElementById('email_reg');
+
+  function explode(){
+    $('#hamb').fadeIn(1000);
+    textUserQuestion.focus();
+    }
+  setTimeout(explode, 500);
+
+  $('.vt').on('click', function(){
+    $('.modal_reg').hide()
+    $('.modal_log').show() 
     function explode(){
-        $('#hamb').fadeIn(1000);
-      }
-      setTimeout(explode, 500);
-    const textUserQuestion = document.getElementById('email_log');
-                textUserQuestion.focus();
-    const textUserQuestion2 = document.getElementById('email_reg');
-    $('.vt').on('click', function(){
-            $('.modal_reg').hide()
-            $('.modal_log').show() 
-            textUserQuestion.focus();
-            
-            
-          });
-    $('.sz').on('click', function(){
-            $('.modal_log').hide()
-            $('.modal_reg').show() 
-            textUserQuestion2.focus();
-          });
+      textUserQuestion.focus();
+    }
+    setTimeout(explode, 500);
+  });
 
+  $('.sz').on('click', function(){
+    $('.modal_log').hide()
+    $('.modal_reg').show() 
+    function explode(){
+      textUserQuestion2.focus();
+    }
+    setTimeout(explode, 500);
+  });
 });
